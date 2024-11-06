99c96440deffb328e7fadb3fa5c2ebfa134a4676
MatshedisoL <mlebakeng0303@gmail.com>
2024-11-06 18:02:27 +0200

    Week 1 Complete

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name")
+age = input("Enter Your Age")
+location = input("Enter Your Location")
+
+print("Hello,", name, "You are", age, "years old.", "And you currently reside in", location, "correct?")
